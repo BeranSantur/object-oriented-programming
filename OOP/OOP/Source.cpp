5a1c2e04f6851d900a7d88f0a3f2c156ad692a3d
Beran Santur <beransantur@gmail.com>
2021-02-16 16:44:42 +0300

    init

---
+++ OOP/OOP/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Dog {
+public:
+	string name;
+	int age;
+
+};
+int main()
+{
+	Dog test_dog;
+	test_dog.name = "Gizemcik";
+	test_dog.age = 19;
+	
+	return 0;
+
+}
